7aebbce27227d7a21f09361ac964adf1b483ac47
Abdelrahman Ahmed Zain alabdeen <147311307+zain-099@users.noreply.github.com>
2026-01-19 15:01:05 +0200

    Fix: Use Custom Prisma Output Path

--- packages/prisma/scripts/db-generate.ts
+++ packages/prisma/scripts/db-generate.ts
@@ -4,15 +4,4 @@ import { executePrismaCommand } from "./executeCommand";
 
 (async () => {
   await executePrismaCommand("prisma generate", { force: true });
-
-  const source = join(__dirname, "../node_modules/.prisma");
-  const dest = join(__dirname, "../../../node_modules/.prisma");
-
-  if (existsSync(source)) {
-    console.log(`Copying Prisma Client from ${source} to ${dest}`);
-    if (!existsSync(dest)) {
-      mkdirSync(dest, { recursive: true });
-    }
-    cpSync(source, dest, { recursive: true, force: true });
-  }
 })();
