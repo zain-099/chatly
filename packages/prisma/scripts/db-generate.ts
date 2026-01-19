af2085e9eb66100489211944f101aebec4b6407d
Abdelrahman Ahmed Zain alabdeen <147311307+zain-099@users.noreply.github.com>
2026-01-19 14:46:04 +0200

    Fix: Use IIFE for db-generate script

--- packages/prisma/scripts/db-generate.ts
+++ packages/prisma/scripts/db-generate.ts
@@ -1,3 +1,18 @@
+import { cpSync, existsSync, mkdirSync } from "fs";
+import { join } from "path";
 import { executePrismaCommand } from "./executeCommand";
 
-executePrismaCommand("prisma generate", { force: true });
+(async () => {
+  await executePrismaCommand("prisma generate", { force: true });
+
+  const source = join(__dirname, "../node_modules/.prisma");
+  const dest = join(__dirname, "../../../node_modules/.prisma");
+
+  if (existsSync(source)) {
+    console.log(`Copying Prisma Client from ${source} to ${dest}`);
+    if (!existsSync(dest)) {
+      mkdirSync(dest, { recursive: true });
+    }
+    cpSync(source, dest, { recursive: true, force: true });
+  }
+})();
